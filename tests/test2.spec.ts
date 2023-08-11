baeece4a6af5bb3762887b253ba55c572316dad8
lukaw3d <lukajeran@oasislabs.com>
2023-08-11 04:28:12 +0200

    Make second test fail

--- tests/test2.spec.ts
+++ tests/test2.spec.ts
@@ -1,5 +1,6 @@
 import { expect, test } from '@playwright/test'
 
-test('test2', async ({ page }) => {
-  expect('a').toBe('a')
+test('test2 (expect fail)', async ({ page }) => {
+  test.fail()
+  await expect(Promise.resolve('a')).resolves.toBe('b')
 })
